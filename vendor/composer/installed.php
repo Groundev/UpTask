dbec46cbf655c1e5380c78023c5dab3708955219
Claudio Martel <claudiomarflo27@gmail.com>
2023-04-20 11:17:59 +0100

    Prueba

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'juandelatorre/up-task_mvc',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'groundev/uptask',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3b38a9642379f190e00a9fb2518650efdd3aa258',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'juandelatorre/up-task_mvc' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'groundev/uptask' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3b38a9642379f190e00a9fb2518650efdd3aa258',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
