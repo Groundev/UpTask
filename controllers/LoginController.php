7ea3f147d6d007f48cde16156c901968970fcf89
Claudio Martel <claudiomarflo27@gmail.com>
2023-04-24 19:42:46 +0100

    Alertas Crear

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Controllers;
 
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController{
@@ -20,13 +21,19 @@ class LoginController{
         echo 'desde logout';
     }
     public static function crear (Router $router){
-        
+        $alertas = [];
+        $usuario = new Usuario;
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarCuentaNueva();
         }
+    
         $router->render('auth/crear', [
-            'titulo' => 'Crear Sesión'
+            'titulo' => 'Crear Sesión',
+            'usuario' => $usuario,
+            'alertas' => $alertas
         ]);
     }
     public static function olvide (Router $router){
--- models/Usuario.php
+++ models/Usuario.php
@@ -9,6 +9,7 @@ class Usuario extends ActiveRecord{
     public $nombre;
     public $email;
     public $password;
+    public $password2;
     public $token;
     public $confirmado;
     public function __construct($args = []){
@@ -16,7 +17,26 @@ class Usuario extends ActiveRecord{
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? null;
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? '0';
+    }
+    public function validarCuentaNueva(){
+        if(!$this->nombre){
+            self::$alertas['error'][] = 'El Nombre del Usuario es Obligatorio';
+        }
+        if(!$this->email){
+            self::$alertas['error'][] = 'El Email del Usuario es Obligatorio';
+        }
+        if(!$this->password){
+            self::$alertas['error'][] = 'La Contraseña es Obligatoria';
+        }
+        if(strlen(!$this->password) < 6){
+            self::$alertas['error'][] = 'La Contraseña debe contener al menos 6 caracteres';
+        }
+        if($this->password !== $this->password2){
+            self::$alertas['error'][] = 'Las Contraseñas son Diferentes';
+        }
+        return self::$alertas;
     }
 }
