6d5386a79a1369370b95c3f14eff7a287c0de61c
Claudio Martel <claudiomarflo27@gmail.com>
2023-04-30 12:26:15 +0100

    Nuevas Interfaces Gestion de Proyectos

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -8,9 +8,33 @@ class DashboardController{
     public static function index (Router $router){
         session_start();
 
+        isAuth();
+
         // Render a la vista
         $router->render('dashboard/index', [
-            'titulo' => 'Iniciar Sesión',
+            'titulo' => 'Proyectos'
+            
+        ]);
+    }
+    public static function crear_proyecto (Router $router){
+        session_start();
+        isAuth();
+        $alertas = [];
+
+        // Render a la vista
+        $router->render('dashboard/crear-proyecto', [
+            'alertas' => $alertas,
+            'titulo' => 'Crear Proyecto'
+            
+        ]);
+    }
+    public static function perfil (Router $router){
+        session_start();
+        isAuth();
+
+        // Render a la vista
+        $router->render('dashboard/perfil', [
+            'titulo' => 'Perfil'
             
         ]);
     }
