2b1c7cb92d41a743e86fce8bcc5584cb3e2ac32c
Groundev <claudiomarflo27@gmail.com>
2023-05-11 22:50:07 +0100

    Eliminar tarea desde Dashboard

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -82,6 +82,30 @@ class TareaController{
     }
     public static function eliminar(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            // Validar que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['proyectoId']);
+
+            session_start();
+
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Error al Actualizar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+            $tarea = new Tarea($_POST);
+            $resultado = $tarea->eliminar();
+
+            $resultado = [
+                'resultado' => $resultado,
+                'mensaje' => 'Eliminado Correctamente',
+                'tipo' => 'exito'
+            ];
+
+            echo json_encode($resultado);
+
             
         }
         
